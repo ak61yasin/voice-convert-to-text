dfca6ea88bc0e596aefb6b5465ee6a41b558f4ce
Kerem Delikmen <keremdlkmn@gmail.com>
2020-06-26 13:50:17 +0300

    Update voice2text file
    Added read and write file operations

--- voice-to-text/voice2text.py
+++ voice-to-text/voice2text.py
@@ -9,8 +9,11 @@ VOICE_FILE_PATH = const_functions.get_video_path("voice2.wav")
 voice_file = voice_operations_functions.read_voice_file(VOICE_FILE_PATH)
 result = voice_operations_functions.voice_to_text(voice_file)
 
+file = read_and_write_file_functions.open_file()
+
 if result != " ":
     toast_messages_functions.create_success_toast_message("Success", "Voice Convert To Text")
+    read_and_write_file_functions.write_file(file, result)
     print(result)
     log_messages_functions.success_log_message("voice2text.py file: Transformed Text Shown")
 else:
