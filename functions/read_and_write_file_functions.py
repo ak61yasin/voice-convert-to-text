da6de4ab3ae8ba0cd2d441ddbfb4f443b527a8e3
Kerem Delikmen <keremdlkmn@gmail.com>
2020-06-26 13:44:44 +0300

    Upload read_and_write_file_functions file
    Makes folder reading and writing operations

---
+++ functions/read_and_write_file_functions.py
@@ -0,0 +1,17 @@
+# Author: Kerem Delikmne
+# Date: 25.06.2020
+# Desc: This function, writes text to the text.txt file
+from functions import log_messages_functions
+
+
+# open_file: Open text.txt file
+def open_file():
+    log_messages_functions.info_log_message("open_file function: Open text.txt file")
+    return open("../utils/data/text.txt", "a", encoding="utf-8")
+
+
+# write_file: Writes text.txt file
+def write_file(file_name, text):
+    log_messages_functions.info_log_message("write_file function: Writes text to text.txt file")
+    log_messages_functions.info_log_message("Text: " + text)
+    file_name.write(text)
