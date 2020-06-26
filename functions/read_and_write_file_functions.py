141cfaa1a276904de5776c433f66a4cbbfa8512e
Kerem Delikmen <keremdlkmn@gmail.com>
2020-06-26 13:58:13 +0300

    Update read_and_write_file_functions file
    read_file functions added

--- functions/read_and_write_file_functions.py
+++ functions/read_and_write_file_functions.py
@@ -15,3 +15,9 @@ def write_file(file_name, text):
     log_messages_functions.info_log_message("write_file function: Writes text to text.txt file")
     log_messages_functions.info_log_message("Text: " + text)
     file_name.write(text)
+
+    
+# read_file: Return the text file in text.txt
+def read_file():
+    with open("../utils/dataset/text.txt", encoding="utf-8") as file:
+        return file.readlines()
